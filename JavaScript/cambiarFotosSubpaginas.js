5df9de0997a38b9d1ccbdc8ce631a2f5ec9b0a06
Fyrthuz <fyrthuz@gmail.com>
2022-03-29 23:23:26 +0200

    Update cambiarFotosSubpaginas.js

--- JavaScript/cambiarFotosSubpaginas.js
+++ JavaScript/cambiarFotosSubpaginas.js
@@ -1,6 +1,10 @@
 const fotos = ["../PortadaHomePage.jpg", "../Noticias/imagen_noticia1.jpeg", "../Noticias/imagen_noticia3.jpeg", "../Noticias/imagen_noticia2.jpeg"];
-for (let i = 0; i < fotos.length; i++) {
-  setTimeout(function(){
-    document.getElementById("imgportada").src=fotos[i];
-  }, 5000*i);
+function recorrerfoto(){
+  for (let i = 0; i < fotos.length; i++) {
+    setTimeout(function(){
+      document.getElementById("imgportada").src=fotos[i];
+    }, 5000*i);
+  }
 }
+recorrerfoto();
+setInterval('recorrerfoto()',21000);
